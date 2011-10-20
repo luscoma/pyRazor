3e7c6b5f875f376bde1ef95a114de99061ab8beb
Alex Lusco <alex.lusco@gmail.com>
2011-10-20 00:44:33 -0400

    still a long way to go but we got somewhere

--- pyrazor.py
+++ pyrazor.py
@@ -3,10 +3,13 @@
 
 import sexylexer
 import re
+from indentstack import IndentStack
 
-PAREN_MATCH = re.compile(r"[()\n]", re.MULTILINE)
+indenter = IndentStack()
+template = ""
 
 def paren_expression(scanner, token):
+  global template
   """Performs paren matching to find the end of a parenthesis expression"""
   start = scanner._position
   plevel = 1
@@ -27,22 +30,75 @@ def paren_expression(scanner, token):
   if plevel != 0:
     raise sexylexer.InvalidTokenError()
   scanner._position = end
+  template += "print " + scanner.input[start:end-1]
   return scanner.input[start-2:end]
+
+def multiline(scanner, token):
+  global template
+  """Handles multiline expressions"""
+  if token == "@:":
+    scanner.ignoreRules = True
+    def pop_multiline():
+      scanner.ignoreRules = False
+    indenter.registerScopeListener(pop_multiline)
+    #TODO(alusco): Handle this case
+  else:
+    template += token[1:]
+  return token
+
+def escaped(scanner, token):
+  global template
+  """Escapes the @ token directly"""
+  template += "\nprint '@'"
+  return "@"
+
+def expression(scanner, token):
+  global template
+  template += "print " + token[1:]
+
+def oneline(scanner, token):
+  global template
+  buzzword = token[:token.index(' ')]
+  if buzzword == "model":
+    template += "# MODEL"
+  else:
+    template += token[1:]
+
+def text(scanner, token):
+  global template
+  template += "print '" + token + "'"
+
+def indent_handler(level):
+  global template
+  template += "\n"
+  template += " " * level
+  indenter.handler(level)
 
 # Parsing rules
 rules = (
-  (r"ESCAPED", r"@@"),
+  (r"ESCAPED", (r"@@", escaped)),
   (r"COMMENT", r"@#.*#@"),
   (r"LINECOMMENT", r"@#.*"),
-  (r"ONELINE", r"@(?:import|from|model) .+$"),
-  (r"MULTILINE", r"@\w*.*:$"),
+  (r"ONELINE", (r"@(?:import|from|model) .+$", oneline)),
+  (r"MULTILINE", (r"@\w*.*:$", multiline)),
   (r"PAREN", (r"@!?\(", paren_expression)),
-  (r"EXPRESSION", r"@!?(\w+(?:(?:\[.+\])|(?:\(.*\)))?(?:\.[a-zA-Z]+(?:(?:\[.+\])|(?:\(.*\)))?)*)"),
-  (r"TEXT", r"[^@\n]+")
+  (r"EXPRESSION", (r"@!?(\w+(?:(?:\[.+\])|(?:\(.*\)))?(?:\.[a-zA-Z]+(?:(?:\[.+\])|(?:\(.*\)))?)*)", expression)),
+  (r"TEXT", (r"[^@\n]+", text))
 )
 
+class View(object):
+  """A base razor view class.  All razor templates inherit from this class"""
+  pass
+
+def render(text):
+  global template;
+  template = ""
+  l = sexylexer.Lexer(rules,indent_handler)
+  for token in l.scan(text):
+    pass
+  print template
+
 # Debug stuff
-l = sexylexer.Lexer(rules)
 def doScan(text):
   for token in l.scan(text):
     print token
--- sexylexer.py
+++ sexylexer.py
@@ -4,132 +4,146 @@
 import re
 
 class TokenError(Exception):
-    """A base exception for use in desginated that an error ocurred
-       when parsing.
-    """
-    def __init__(self, token, lineno):
-      self.token = token
-      self.lineno = lineno
+  """A base exception for use in desginated that an error ocurred
+     when parsing.
+  """
+  def __init__(self, token, lineno):
+    self.token = token
+    self.lineno = lineno
 
-    def __str__(self):
-        return "Line #%s, Found token: %s" % (self.lineno, self.token)
+  def __str__(self):
+    return "Line #%s, Found token: %s" % (self.lineno, self.token)
 
 class InvalidTokenError(TokenError):
-    """This exception is for use by parsing functions to designate an
-       error has occurred when parsing out a token.  When thrown within
-       a parsing function it will be caught by the parser and rethrown
-       with lineno and the matched portion of the token.
-    """
-    def __init__(self, token = None, lineno = None):
-      """A constructor that can be used by callbacks to signify an
-         error.  This will be caught and reraised with position and
-         line number information"""
-      TokenError.__init__(self, token, lineno)
+  """This exception is for use by parsing functions to designate an
+     error has occurred when parsing out a token.  When thrown within
+     a parsing function it will be caught by the parser and rethrown
+     with lineno and the matched portion of the token.
+  """
+  def __init__(self, token = None, lineno = None):
+    """A constructor that can be used by callbacks to signify an
+       error.  This will be caught and reraised with position and
+       line number information"""
+    TokenError.__init__(self, token, lineno)
 
 class UnknownTokenError(TokenError):
-    """ This exception is for use to be thrown when an unknown token is
-        encountered in the token stream. It holds the line number and the
-        offending token.
-    """
-    pass
- 
+  """ This exception is for use to be thrown when an unknown token is
+      encountered in the token stream. It holds the line number and the
+      offending token.
+  """
+  pass
+
 class _InputScanner(object):
-    """ This class manages the scanning of a specific input. An instance of it is
-        returned when scan() is called. It is built to be great for iteration. This is
-        mainly to be used by the Lexer and ideally not directly.
+  """ This class manages the scanning of a specific input. An instance of it is
+      returned when scan() is called. It is built to be great for iteration. This is
+      mainly to be used by the Lexer and ideally not directly.
+  """
+
+  def __init__(self, lexer, input):
+    """ Put the lexer into this instance so the callbacks can reference it 
+        if needed.
     """
- 
-    def __init__(self, lexer, input):
-        """ Put the lexer into this instance so the callbacks can reference it 
-            if needed.
-        """
-        self._position = 0
-        self.lexer = lexer
-        self.input = input
-        self.regex_nline = re.compile("[\r]?[\n](\s*)")
- 
-    def __iter__(self):
-        """ All of the code for iteration is controlled by the class itself.
-            This and next() (or __next__() in Python 3.0) are so syntax
-            like `for token in Lexer(...):` is valid and works.
-        """
-        return self
- 
-    def next(self):
-        """ Used for iteration. It returns token after token until there
-            are no more tokens. (change this to __next__(self) if using Py3.0)
-        """
-        if not self.done_scanning():
-            return self.scan_next()
-        raise StopIteration
- 
-    def done_scanning(self):
-        """ A simple boolean function that returns true if scanning is
-            complete and false if it isn't.
-        """
-        return self._position >= len(self.input)
- 
-    def scan_next(self):
-        """ Retreive the next token from the input. If the
-            flag `omit_whitespace` is set to True, then it will
-            skip over the whitespace characters present.
-        """
-        if self.done_scanning():
-            return None
+    self._position = 0
+    self.lexer = lexer
+    self.input = input
+    self.regex_nline = re.compile("[\r]?[\n](\s*)")
+    self.ignoreRules = False
+    self.regex_line = re.compile(".+$", re.MULTILINE)
 
-        # Match \r and \n, determine indent, and move past them
-        match = self.regex_nline.match(self.input, self._position)
-        if match is not None:
-          # len(match.group(1)) holds the indent level
-          self._position = match.end()
-          return "INDENT", len(match.group(1))
+  def __iter__(self):
+    """ All of the code for iteration is controlled by the class itself.
+        This and next() (or __next__() in Python 3.0) are so syntax
+        like `for token in Lexer(...):` is valid and works.
+    """
+    return self
 
-        # Try to match a token
-        match = self.lexer.regexc.match(self.input, self._position)
-        if match is None:
-            lineno = self.input[:self._position].count("\n") + 1
-            raise UnknownTokenError(self.input[self._position], lineno)
+  def next(self):
+    """ Used for iteration. It returns token after token until there
+        are no more tokens. (change this to __next__(self) if using Py3.0)
+    """
+    if not self.done_scanning():
+      return self.scan_next()
+    raise StopIteration
 
-        self._position = match.end()
-        value = match.group(match.lastgroup)
+  def done_scanning(self):
+    """ A simple boolean function that returns true if scanning is
+        complete and false if it isn't.
+    """
+    return self._position >= len(self.input)
 
-        # Callback
-        if match.lastgroup in self.lexer._callbacks:
-            try:
-                value = self.lexer._callbacks[match.lastgroup](self, value)
-            except InvalidTokenError:
-                # raise with some actual information
-                lineno = self.input[:self._position].count("\n") + 1
-                raise InvalidTokenError(self.input[self._position], lineno)
-        return match.lastgroup, value
- 
- 
+  def indent_handler(self, level):
+    if self.lexer.indent_handler is not None:
+      self.lexer.indent_handler(level)
+    return "INDENT", level
+
+  def scan_next(self):
+    """ Retreive the next token from the input. If the
+        flag `omit_whitespace` is set to True, then it will
+        skip over the whitespace characters present.
+    """
+    if self.done_scanning():
+        return None
+
+    # Match \r and \n, determine indent, and move past them
+    match = self.regex_nline.match(self.input, self._position)
+    if match is not None:
+      # len(match.group(1)) holds the indent level
+      self._position = match.end()
+      return self.indent_handler(len(match.group(1).replace("\t","  ")))
+
+    # If ignore rules
+    if self.ignoreRules:
+      match = self.regex_line.match(self.input, self._position)
+      self._position = match.end()
+      return "LINE", match.group()
+
+    # Try to match a token
+    match = self.lexer.regexc.match(self.input, self._position)
+    if match is None:
+      lineno = self.input[:self._position].count("\n") + 1
+      raise UnknownTokenError(self.input[self._position], lineno)
+
+    self._position = match.end()
+    value = match.group(match.lastgroup)
+
+    # Callback
+    if match.lastgroup in self.lexer._callbacks:
+      try:
+        value = self.lexer._callbacks[match.lastgroup](self, value)
+      except InvalidTokenError:
+        # raise with some actual information
+        lineno = self.input[:self._position].count("\n") + 1
+        raise InvalidTokenError(self.input[self._position], lineno)
+    return match.lastgroup, value
+
 class Lexer(object):
-    """ A lexical scanner. It takes in an input and a set of rules based
-        on reqular expressions. It then scans the input and returns the
-        tokens one-by-one. It is meant to be used through iterating.
+  """ A lexical scanner. It takes in an input and a set of rules based
+      on reqular expressions. It then scans the input and returns the
+      tokens one-by-one. It is meant to be used through iterating.
+  """
+
+  def __init__(self, rules, indent_handler=None, case_sensitive=False):
+    """ Set up the lexical scanner. Build and compile the regular expression
+        and prepare the whitespace searcher.
     """
- 
-    def __init__(self, rules, case_sensitive=False):
-        """ Set up the lexical scanner. Build and compile the regular expression
-            and prepare the whitespace searcher.
-        """
-        self._callbacks = {}
-        self.case_sensitive = case_sensitive
-        parts = []
-        for name, rule in rules:
-            if not isinstance(rule, str):
-                rule, callback = rule
-                self._callbacks[name] = callback
-            parts.append("(?P<%s>%s)" % (name, rule))
-        if self.case_sensitive:
-            flags = re.M
-        else:
-            flags = re.M|re.I
-        self.regexc = re.compile("|".join(parts), flags)
- 
-    def scan(self, input):
-        """ Return a scanner built for matching through the `input` field. 
-            The scanner that it returns is built well for iterating.
-        """
-        return _InputScanner(self, input)
+    self._callbacks = {}
+    self.case_sensitive = case_sensitive
+    parts = []
+    for name, rule in rules:
+      if not isinstance(rule, str):
+        rule, callback = rule
+        self._callbacks[name] = callback
+        parts.append("(?P<%s>%s)" % (name, rule))
+      if self.case_sensitive:
+        flags = re.M
+      else:
+        flags = re.M|re.I
+
+    self.indent_handler = indent_handler
+    self.regexc = re.compile("|".join(parts), flags)
+
+  def scan(self, input):
+    """ Return a scanner built for matching through the `input` field. 
+        The scanner that it returns is built well for iterating.
+    """
+    return _InputScanner(self, input)
