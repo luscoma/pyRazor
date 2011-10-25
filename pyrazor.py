9c0b9ebe59300514083efaacb7ce84c3be0156d8
Alex Lusco <alex.lusco@gmail.com>
2011-10-24 22:58:24 -0400

    Refactored how a view is rendered

--- pyrazor.py
+++ pyrazor.py
@@ -5,6 +5,12 @@ from lex import RazorLexer, Token
 from razorview import View
 
 def render(text, model=None, debug=False):
+  """Renders a template given the template text"""
+  view = _buildView(text, debug)
+  return view.render(model)
+
+def _buildView(text, debug=False):
+  """Parses text building a view"""
   lex = RazorLexer.create()
   view = View()
   for token in lex.scan(text):
@@ -12,4 +18,4 @@ def render(text, model=None, debug=False):
       print token
     view.parseToken(lex.getScope(), token)
   view.build(debug=debug)
-  return view.render(model)
+  return view
