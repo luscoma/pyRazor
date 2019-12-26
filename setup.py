ba6f8732c383c2c2d2cb88a484c1007d103a5483
Hosein Yeganloo <hosein@lappi.yeganloo.net>
2019-12-26 05:55:01 +0330

    Work on packaging. Tests broken!

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyRazor',
+    version='0.0.1',
+    description='A python implementation of the razor engine',
+    url='git@github.com:luscoma/pyRazor.git',
+    author='Alex Lusco',
+    author_email="",
+    license='MIT',
+    packages=['pyRazor'],
+    zip_safe=False
+)
